66df044ec26f67e207ec24a1b8b740b03637506d
Ahmad Farag <afarag@cas.org>
2018-01-29 10:00:53 -0500

    Adding Rest Stuff

---
+++ rest/src/main/java/com/stacktrace/yo/seed/restboot/BasicRestBootApplication.java
@@ -0,0 +1,12 @@
+package com.stacktrace.yo.seed.restboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicRestBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicRestBootApplication.class, args);
+	}
+}
---
+++ rest/src/test/java/com/stacktrace/yo/seed/restboot/BasicRestBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stacktrace.yo.seed.restboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicRestBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
